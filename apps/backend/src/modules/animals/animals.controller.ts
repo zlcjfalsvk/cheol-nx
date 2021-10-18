d85efd2ca9f35a306b6806ef5960b6b8c00ab53e
cheolmin choi <zl0759vk@naver.com>
2021-10-18 20:49:56 +0900

    feat(apps/backend): 뼈대 구축

--- apps/backend/src/modules/animals/animals.controller.ts
+++ apps/backend/src/modules/animals/animals.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import type { ApiResponseInterface } from '@cheol-nx/api-interfaces';
+
+import { ApiResponseDto } from '../../api-response.dto';
 
 @Controller('animals')
-export class AnimalsController {}
+export class AnimalsController {
+  @Get()
+  hellow(): ApiResponseInterface<string> {
+    return new ApiResponseDto({
+      data: 'hellow',
+    });
+  }
+}
