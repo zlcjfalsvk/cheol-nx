d783c27c9a0a79dbe81f8d069c0c77d0c082d053
cheolmin choi <zl0759vk@naver.com>
2021-10-18 22:21:31 +0900

    refactor(apps/backend): backend -> backend/basic 변경

--- apps/backend/basic/jest.config.js
+++ apps/backend/basic/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'backend',
-  preset: '../../jest.preset.js',
+  displayName: 'backend-basic',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/backend',
+  coverageDirectory: '../../../coverage/apps/backend/basic',
 };
